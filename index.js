057f6cf76df4f569c6e9580b6c5da8c8f8ffcbde
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-20 02:35:50 -0500

    cleared commented code

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
 // index.js
 const { nextISSTimesForMyLocation } = require('./iss');
-// let ipadress = "";
-// let coordinates = { latitude: '49.27670', longitude: '-123.13000' };
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:' , ip);
-//   ipadress = ip;
-// });
-
-// fetchCoordsByIP(ipadress,(error, data) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   coordinates = data;
-//   console.log('Data is:' , data);
-// });
-
-// fetchISSFlyOverTimes(coordinates,(error,passTimes)=> {
-//   if (error) {
-//     console.log("It didnt work " , error);
-//     return;
-//   }
-//   console.log('Flyoverpass time is',passTimes);
-
-// });
 
 nextISSTimesForMyLocation((error,PassTimes) => {
   if (error) {
